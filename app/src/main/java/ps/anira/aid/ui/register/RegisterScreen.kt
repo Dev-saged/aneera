a1b7902ed393c7702f6f9bada9f92d96de46b637
Dev-saged <109200966+Dev-saged@users.noreply.github.com>
2026-08-04 00:40:02 +0300

    Update RegisterScreen.kt

--- app/src/main/java/ps/anira/aid/ui/register/RegisterScreen.kt
+++ app/src/main/java/ps/anira/aid/ui/register/RegisterScreen.kt
@@ -2,6 +2,9 @@ package ps.anira.aid.ui.register
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
